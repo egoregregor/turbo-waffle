e1d9371919e510a451b9b7a0f6fc54beead1296d
Egor <157018655+egoregregor@users.noreply.github.com>
2024-01-18 17:51:35 +0500

    Add files via upload

---
+++ turbo-waffle.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         cc anti-cheat
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-18
+// @description  remove fill-solution button
+// @author       egor
+// @match        https://codecombat.com/play/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=codecombat.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function removeButton() {
+        var fillSolutionButton = document.getElementById('fill-solution');
+        if (fillSolutionButton) {
+            fillSolutionButton.parentNode.removeChild(fillSolutionButton);
+        }
+    }
+
+    setInterval(removeButton, 1);
+})();
